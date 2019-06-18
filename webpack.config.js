dcbcc8456f68b7a4e61edf2e1325ab14a790b260
Puspa <gpuspamail@gmail.com>
2019-06-18 13:09:23 +0200

    setting up webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,9 @@
-module.exports = {};
+const path = require('path');
+
+module.exports = {
+    entry: './src/js/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist/js'),
+        filename: 'bundle.js'
+    }
+};
