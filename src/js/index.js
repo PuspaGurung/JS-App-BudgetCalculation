499b43dfa10237ad75936a91232abc471979cdbc
Puspa <gpuspamail@gmail.com>
2019-07-15 01:44:25 +0200

    ass stylesheet, some changes on HTML elements

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,12 @@
 import '../scss/style.scss';
 
-document.getElementsByClassName('cancel-entry').addEventListener('click', () => {
-    console.log('hi there');
+document.querySelector('.cancel-entry').addEventListener('click', function () {
+    let dd = document.querySelector('#pop-up__box');
+    dd.style.visibility = 'hidden';
 });
 
+function addIncome() {
+    console.log('hi');
+}
+
+addIncome();
